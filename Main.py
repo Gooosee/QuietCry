c16dfbb6a5a02caa5da9d37c9c638c926463cfa7
Gooosee <46865601+Gooosee@users.noreply.github.com>
2020-01-05 15:45:42 +0300

    Новое изображение пули, оптимизация анимации, в случае, если будет анимированный объект, кроме персонажа. Примитивная, но рабочая стрельба, с удалением, в случае выхода за границу пуль. Мелкие доработки.

--- Main.py
+++ Main.py
@@ -5,13 +5,13 @@ pygame.init()
 # создание окна
 size = width, height = 800, 800
 screen = pygame.display.set_mode(size)
-cur_frame = 0
 
 # Задержки для анимаций
 
 waitStop = [100, 100, 100]  # Стоя
 waitRun = [100, 150, 100]  # Набегу
 waitJump = [100, 100, 100, 100, 100, 100]  # В прыжке
+waitFire = [100, 100, 100, 100, 100, 100, 100, 100, 100]
 
 personRun = [LoadImage.load_image('anim1_person_run_m4a1s.png', 'data'),
              LoadImage.load_image('anim2_person_run_m4a1s.png', 'data'),
@@ -65,63 +65,68 @@ personStopLeft = [pygame.transform.flip(personStop[0], True, False),
                   pygame.transform.flip(personStop[2], True, False),
                   pygame.transform.flip(personStop[3], True, False)]
 
+bull = LoadImage.load_image('bullet.png', 'data')
 person_sprites = pygame.sprite.GroupSingle()
 all_sprites = pygame.sprite.Group()
 tile_sprites = pygame.sprite.Group()
+bullet_sprites = pygame.sprite.Group()
 direction = False
 
 
-class AnimatedSprite(pygame.sprite.Sprite):
-    def __init__(self, frames, x, y, num_wait):
-        super().__init__(person_sprites)
-        self.num_wait = num_wait
-        global cur_frame
-        self.frames = frames
-        if cur_frame >= len(self.frames):
-            cur_frame = 0
-        self.image = self.frames[cur_frame]
+class Person(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__(all_sprites)
+        # Начальные координаты персонажа
+        self.pos_x = x
+        self.pos_y = y
+
+        self.cur_frame = 0  # Номер кадра
+        self.frames = personStop
+        self.num_wait = waitStop
+        self.image = personStop[self.cur_frame]  # Изображение спрайта
         self.rect = self.image.get_rect()
-        self.rect = self.rect.move(x, y)
-
-    def wait(self):
-        pygame.time.wait(self.num_wait[cur_frame - 1])
-
-    def update(self):
-        global cur_frame
-        self.wait()
-        self.image = self.frames[cur_frame]
-        cur_frame += 1
-        print(cur_frame)
-
-
-class Person:
-    def __init__(self, pos_x, pos_y):
-        self.anim = AnimatedSprite
-        self.pos_x = pos_x
-        self.pos_y = pos_y
+        self.rect = self.rect.move(self.pos_x, self.pos_y)
 
     def run(self, keys):  # Бег
         global direction
         if keys[pygame.K_LEFT]:
-            self.pos_x = self.pos_x - 15
-            self.anim(personRunLeft, self.pos_x - 30, self.pos_y, waitRun).update()
-            direction = False  # Персонаж смотрит влево
+            self.frames = personRunLeft
+            self.num_wait = waitRun
+            self.rect.x = self.pos_x - 40
+            self.pos_x -= 15
+            direction = True  # Персонаж смотрит влево
         elif keys[pygame.K_RIGHT]:
-            self.pos_x = self.pos_x + 15
-            self.anim(personRun, self.pos_x, self.pos_y, waitRun).update()
-            direction = True  # Персонаж смотрит вправо
+            self.frames = personRun
+            self.num_wait = waitRun
+            self.pos_x += 15
+            direction = False  # Персонаж смотрит вправо
+        self.rect.x = self.pos_x
 
     def stop(self):  # Отсутствие движения
         if direction:
-            self.anim(personStop, self.pos_x, self.pos_y, waitStop).update()
+            self.frames = personStopLeft
+            self.num_wait = waitStop
         else:
-            self.anim(personStopLeft, self.pos_x - 30, self.pos_y, waitStop).update()
+            self.frames = personStop
+            self.num_wait = waitStop
 
     def jump(self):  # Прыжок
         pass
 
     def fire(self):  # Стрельба
-        pass
+        if direction:
+            self.frames = personFireLeft
+            self.rect.x = self.pos_x - 5
+            self.num_wait = waitFire
+        else:
+            self.frames = personFire
+            self.rect.x = self.pos_x - 5
+            self.num_wait = waitFire
+        bul = Bullet(self.pos_x + 110, self.pos_y + 36, direction)
+
+    def wait(self):
+        print(self.cur_frame)
+        pygame.time.wait(self.num_wait[self.cur_frame - 1])
 
     def update(self):
         keys = pygame.key.get_pressed()  # в этом списке лежат все нажатые кнопки
@@ -133,11 +138,35 @@ class Person:
             self.fire()
         else:
             self.stop()  # отсутствие движения
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.wait()
+        self.image = self.frames[self.cur_frame - 1]
 
-# Начальное положение персонажа
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y, direction_bul):
+        super().__init__(bullet_sprites, all_sprites)
+        self.x, self.y, self.direction_bul = x, y, direction_bul
+        self.image = bull
+        self.rect = self.image.get_rect()
+        if direction_bul:
+            self.x -= 115
+        self.rect = self.rect.move(self.x, self.y)
+
+    def update(self, *args):
+        if not self.direction_bul:
+            if self.rect.x <= 600:
+                self.rect.x += 40
+            else:
+                self.kill()
+        else:
+            if self.rect.x >= 0:
+                self.rect.x -= 40
+            else:
+                self.kill()
 
 
-pers = Person(500, 500)
+pers = Person(500, 500)  # Начальное положение персонажа
 
 
 def main():  # главная функция
@@ -147,10 +176,10 @@ def main():  # главная функция
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        pers.update()
         screen.fill((0, 0, 0))
-        person_sprites.draw(screen)
-        clock.tick(50)
+        all_sprites.draw(screen)
+        all_sprites.update()
+        clock.tick(500)
         pygame.display.flip()
 
 
